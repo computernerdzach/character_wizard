d77b735da56959434549dcd9d0ffd3c87f9b2035
zach <wizteczach@gmail.com>
2021-12-22 13:37:13 -0800

    cleans up attribute import

--- races/gnome.py
+++ races/gnome.py
@@ -2,8 +2,17 @@ from races.race import Race
 
 
 class Gnome(Race):
-    speed = 25
-    racial_bonuses = {'Con': 1}
+    def __init__(self):
+        self.speed = 25
+        self.racial_bonuses = {'INT': 2}
+        self.size = 'small'
+        self.languages = ['gnomish']
+        self.sub_races.append('rock-gnome', 'deep-gnome')
+        self.traits.append('darkvision', 'gnome-cunning')
+        for tongue in Race.languages:
+            self.languages.append(tongue)
+
+    sub_races = ('Rock-Gnome', 'Deep-Gnome')
 
     def __str__(self):
         return 'Gnome'
