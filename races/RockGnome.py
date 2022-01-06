669f5a81f2dd4778c68f78430391a576263112da
zach <ztheilen@coppertreeanalytics.com>
2022-01-06 15:02:25 -0800

    corrects missing trait

--- races/RockGnome.py
+++ races/RockGnome.py
@@ -5,6 +5,8 @@ class RockGnome(Gnome):
     def __init__(self):
         super().__init__()
         self.racial_bonuses['con'] = 1
+        self.traits.append("artificers-lore")
+        self.traits.append('tinker')
 
     def __str__(self):
         return 'Rock Gnome'
